565a08835511ec83fc2543e68cc5be56b24c612a
shs <son809616@gmail.com>
2023-04-14 10:15:04 +0900

    1

--- calc.py
+++ calc.py
@@ -1,25 +1,57 @@
 # get two integer parameters
 # return sum
-def plus(x, y):
+def ca1(x, y):
     return x+y
 
+def ca2(x, y):
+    return x-y
+
+def ca3(x,y):
+    return x*y
+
+def ca4(x,y):
+    try:
+        result = x/y
+        return result
+    except ZeroDivisionError:
+        print("0으로 나눌 수 없습니다")
 # main function
 def main():
     check = 1
     print("Welcome to calcuator")
     while check >= 1:        
-        print("0: exit, 1: plus")
-        check = int(input())
+        print("0: exit, 1: plus 2: minus 3: multiplication 4: division")
+        check = int(input("입력:"))
         if check == 1:
             print("First Number")
-            x = int(input())
+            x = int(input("입력:"))
             print("Second Number")
-            y = int(input())
-            print("answer : ", plus(x,y))
-        elif check > 1:
+            y = int(input("입력:"))
+            print("answer : ", ca1(x,y))
+        elif check == 2:
+            print("First Number")
+            x = int(input("입력:"))
+            print("Second Number")
+            y = int(input("입력:"))
+            print("answer : ", ca2(x,y))
+        elif check == 3:
+            print("First Number")
+            x = int(input("입력:"))
+            print("Second Number")
+            y = int(input("입력:"))
+            print("answer : ", ca3(x,y))
+        elif check == 4:
+            print("First Number")
+            x = int(input("입력:"))
+            print("Second Number")
+            y = int(input("입력:"))
+            print("answer : ", ca4(x,y))
+        elif check > 4 or check < 0:
             print("Unsupported")
         else:
             print("Thank you")
+            
+                
 
 if __name__ == "__main__":
     main()
